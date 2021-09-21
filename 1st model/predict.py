5c7bde43d671c649b902a8df0cae4260bd459b4c
Hrugved Kolhe <geeky.hrugved143@gmail.com>
2021-09-22 02:27:08 +0530

    predict model

--- 1st model/predict.py
+++ 1st model/predict.py
@@ -14,6 +14,11 @@ file3 = open('./model3.pkl', 'rb')
 clf3 = pickle.load(file3)
 file3.close()
 
+file4 = open('./model4.pkl', 'rb')
+clf4 = pickle.load(file4)
+file4.close()
+
+
 userdata = [[sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12], sys.argv[13], sys.argv[14], sys.argv[15], sys.argv[16], sys.argv[17], sys.argv[18], sys.argv[19],sys.argv[20],sys.argv[21] ]]
 
 # Prediction By Decision Tree
@@ -31,6 +36,12 @@ print(clf3.predict(userdata))
 classprobs3 = clf3.predict_proba(userdata)
 predclassprob3 = np.max(classprobs3)
 
+# Prediction By XGBoost
+print(clf4.predict(userdata)) 
+classprobs4 = clf4.predict_proba(userdata)
+predclassprob4 = np.max(classprobs4)
+
 print(predclassprob1)
 print(predclassprob2)
 print(predclassprob3)
+print(predclassprob4)
